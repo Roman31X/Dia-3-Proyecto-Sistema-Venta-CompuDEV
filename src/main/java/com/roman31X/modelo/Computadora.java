7f163f8419d84217d69103e277e8102472533b30
Roman31X <die.haa65@gmail.com>
2024-06-12 15:00:00 -0500

    Computadoras agregado con exito

---
+++ src/main/java/com/roman31X/modelo/Computadora.java
@@ -0,0 +1,33 @@
+package com.roman31X.modelo;
+
+public class Computadora {
+    private int idComputadora;
+    private String nombre;
+    private Monitor monitor;
+    private Teclado teclado;
+    private Raton raton;
+    private static int contadorComputadoras;
+
+    private Computadora() {
+        idComputadora = ++contadorComputadoras;
+    }
+
+    public Computadora(String nombre, Monitor monitor, Teclado teclado, Raton raton) {
+        this();
+        this.nombre = nombre;
+        this.monitor = monitor;
+        this.teclado = teclado;
+        this.raton = raton;
+    }
+
+    @Override
+    public String toString() {
+        return "Computadora: [" +
+                "ID Computadora: [" + idComputadora +
+                "] Nombre: [" + nombre +
+                "] Monitor: [" + monitor +
+                "] Teclado: [" + teclado +
+                "] Raton: [" + raton +
+                ']';
+    }
+}
